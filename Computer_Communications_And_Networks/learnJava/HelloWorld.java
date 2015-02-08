4853ab2aa55431799df3d904c0641c91b682184f
akuisara <zhang_qi@wheatoncollege.edu>
2015-02-08 12:19:46 +0000

    added superclass/subclass notes

--- Computer_Communications_And_Networks/learnJava/HelloWorld.java
+++ Computer_Communications_And_Networks/learnJava/HelloWorld.java
@@ -1,17 +1,36 @@
 // Java API Documentation: http://docs.oracle.com/javase/7/docs/api/
+
 // Garbage collection is a part of JVM, the garbage collector runs its own thread, references expire when out of the scope, explicitly expire the persistent objects by setting to null
 // Variables: primitives ((data type lowercase / helper class uppercase (e.g., upward numeric conversion, which can also be done by using the casting syntax: add (certainType) before the value)) - int (byte, short, int, long: num_L), char, bool, float (float: num_f, double: num_d)); complex objects (string, date, other objects...). Class variables are fields
 // C-style language with similar operators. For/while loop, if/switch statements, overloading functions are the same as C++
 // Variables and strings are always pass-by-copy, complex objects like an array pass by reference because copy and original point to the same memory location
+
+// package helpers; write at the top of the helper functions in a helpers folder
+// import helpers.*; after created the helper functions
+// inside an instance class, namefield "this" is current instance in current class
+// getExample/setExample as getter/setter for Example
+// public class Subclass extends Superclass {}
+// overload a constructor can call super() from inheritance superclass
+// add "@Override" before overriding the functions from superclass makes code more readable
+// downward casting: Subclass subInstance = (Subclass) parentMethod
+
+// create instance import java.util.Collection
+// public interface SomeInterface {method can only be public or abstract, where abstract functions must be implemented in subclasses} 
+// public class SomeClass implement SomeInterface { @Override methods} 
+
 // dummynet: network emulation tool
+
 
 import java.math.*; // BigDecimal
 import java.io.*;
-import java.util.*; // Date, GregorianCalendar
+import java.util.*; // Date, GregorianCalendar, ArrayList, HashMap, Set, Iterator, ListIterator
 import java.text.*; // DateFormat
 import java.net.*;  // URI, URL
 
 public class HelloWorld {
+    // constant variable: final the value cannot be changed
+    public static final long RED = 0xff0000;
+
     public static void main(String[] args) {
         // public accessible
         // static does not have to create an instance
@@ -80,7 +99,30 @@ public class HelloWorld {
 
         } catch (Exception e){
             System.out.println(e.getMessage());
-        }        
+        }     
+
+        // multidimensional arrays
+        // String [][] states = new String[int][int];   
+        ArrayList<String> statesList = new ArrayList<String>();
+        statesList.add("N/A");
+        statesList.add("MA"); statesList.add("CO"); statesList.add("FL"); statesList.add("NH"); statesList.add("RI"); statesList.add("NY");
+        statesList.remove(0); // get(), indexOf(), etc
+        System.out.println(statesList);
+        ListIterator<String> listIterator = statesList.listIterator();
+        while (listIterator.hasNext()){
+            System.out.println(listIterator.next());
+        }
+
+        HashMap<String, String> hashMap = new HashMap<String, String>();
+        hashMap.put("Massachusetts", "MA"); // remove(key), get(key), etc
+        hashMap.put("New York", "NY"); // remove(key), get(key), etc
+        System.out.println(hashMap);
+        Set<String> keys = hashMap.keySet();
+        Iterator<String> iterator = keys.iterator();
+        while (iterator.hasNext()){
+            String state = iterator.next();
+            System.out.println(state + ": " + hashMap.get(state));
+        }
     }
 
     private static String getInput(String prompt){
