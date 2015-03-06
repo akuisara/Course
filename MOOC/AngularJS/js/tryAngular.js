909293190d791a7de402a133967336bc73e72159
Qi Zhang <zhang_qi@wheatoncollege.edu>
2015-03-06 17:53:06 +0000

    add second partial

--- MOOC/AngularJS/js/tryAngular.js
+++ MOOC/AngularJS/js/tryAngular.js
@@ -10,5 +10,17 @@ exampleController.controller("DetailsController", ["$scope", "$http", "$routePar
 	$http.get("js/example.json").success(function(data){
 		$scope.choices = data;
 		$scope.whichChoice = $routeParams.itemInstruction;
+
+		if ($routeParams.itemInstruction > 0) {
+			$scope.prevItem = Number($routeParams.itemInstruction) - 1;
+		} else {
+			$scope.prevItem = $scope.choices.length - 1;
+		}
+
+		if ($routeParams.itemInstruction < ($scope.choices.length - 1)) {
+			$scope.nextItem = Number($routeParams.itemInstruction) + 1;
+		} else {
+			$scope.nextItem = 0;
+		}
 	});
 }]);
